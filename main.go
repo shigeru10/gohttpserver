bb72cc1ae5b0075107b14d1f539b20a095abd535
shigeru10 <shiiiiigeeeee10@gmail.com>
2018-08-07 12:05:56 +0900

    add dynamic page

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
+func clockHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,
+		`<!DOCTYPE html>
+    <html>
+    <body>
+      It's %d o'clock now.
+    </body>
+    </html>
+  `, time.Now().Hour())
+}
+
 func main() {
+	http.HandleFunc("/clock", clockHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./doc"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
