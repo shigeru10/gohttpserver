15e36015c2e8b10d56a225a55b27e1557a90191b
shigeru10 <shiiiiigeeeee10@gmail.com>
2018-08-07 12:22:18 +0900

    add json response

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,7 +16,23 @@ func clockHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func apiClockHandler(w http.ResponseWriter, r *http.Request) {
+	type ResponseBody struct {
+		Time time.Time `json:"time"`
+	}
+	rb := &ResponseBody{
+		Time: time.Now(),
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(rb); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	http.HandleFunc("/api/clock", apiClockHandler)
 	http.HandleFunc("/clock", clockHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./doc"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
